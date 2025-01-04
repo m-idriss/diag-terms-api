4552947b84e2f6597ef09a7769b980ef7cf5f08a
Idriss MOHAMADY <pro.mohamady@gmail.com>
2025-01-04 22:41:29 +0100

    add index.html

--- src/test/java/com/dime/StaticContentTest.java
+++ src/test/java/com/dime/StaticContentTest.java
@@ -1,22 +1,32 @@
 package com.dime;
 
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
 
 @QuarkusTest
 class StaticContentTest {
 
-  @ConfigProperty(name = "quarkus.application.name")
-  String appName;
+  @TestHTTPResource("index.html")
+  URL url;
+
+  @TestHTTPResource("3dime-logo-blue-small.png")
+  URL smallLogoUrl;
+
+  @TestHTTPResource("script.js")
+  URL scriptUrl;
+
+  @ConfigProperty(name = "quarkus.swagger-ui.path")
+  String swaggerPath;
 
   @Test
   void testBrandingLogoExists() throws IOException {
@@ -27,13 +37,44 @@ class StaticContentTest {
   }
 
   @Test
-  @Deprecated
-  void testRootUrlShowsSwaggerUI() {
-    given()
-        .when().get("/")
-        .then()
-        .statusCode(200)
-        .body(containsString("swaggerUiLogoLink"))
-        .body(containsString(appName));
+  void testIndexHtmlContainsLogoSmall() throws IOException {
+    try (InputStream in = url.openStream()) {
+      String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+      Assertions.assertTrue(contents.contains(String.format("\".%s\" alt=\"logo\"", smallLogoUrl.getFile())));
+    }
+  }
+
+  @Test
+  void testIndexHtmlContainsSwaggerUiPath() throws IOException {
+    try (InputStream in = url.openStream()) {
+      String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+      Assertions.assertTrue(contents.contains(String.format("href=\"%s\"", swaggerPath)));
+    }
+  }
+
+  @Test
+  void testScriptJsExists() throws IOException {
+    try (InputStream in = scriptUrl.openStream()) {
+      byte[] bytes = in.readAllBytes();
+      Assertions.assertTrue(bytes.length > 0);
+    }
+  }
+
+  @Test
+  void testIndexHtmlContainsScriptJs() throws IOException {
+    try (InputStream in = url.openStream()) {
+      String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+
+      Assertions.assertTrue(contents
+          .contains(String.format("<script src=\"%s\"></script>", new File(scriptUrl.getFile()).getName())));
+    }
+  }
+
+  @Test
+  void testScriptJsContainsApiCall() throws IOException {
+    try (InputStream in = scriptUrl.openStream()) {
+      String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+      Assertions.assertTrue(contents.contains(swaggerPath + "/v1/terms"));
+    }
   }
 }
